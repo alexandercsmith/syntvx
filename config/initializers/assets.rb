463c2679c74bb57be05d6405148b890b1c16d7f3
Alexander Smith <acsmith.dev@gmail.com>
2018-12-21 13:44:43 -0500

    add columns and mixins

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
+# TODO |CONFIG| Update Asset Version if Changes before Deployment
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
