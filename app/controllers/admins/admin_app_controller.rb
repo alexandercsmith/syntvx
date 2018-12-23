8a087ef0ce430f8e15d6741d30f9f063e8f511d9
Alexander Smith <acsmith.dev@gmail.com>
2018-12-22 21:22:47 -0500

    update app controller

--- app/controllers/admins/admin_app_controller.rb
+++ app/controllers/admins/admin_app_controller.rb
@@ -1,4 +1,12 @@
 class Admins::AdminAppController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :redirect_admin
   layout 'admin'
+
+  private
+
+    def redirect_admin
+      unless current_admin
+        redirect_to root_path
+      end
+    end
 end
