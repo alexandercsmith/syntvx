d7b9a0cbd99aefe9e59077c4b03faef218479d67
Alexander Smith <acsmith.dev@gmail.com>
2018-12-22 21:23:16 -0500

    authenticate admin

--- app/controllers/admins/admin_app_controller.rb
+++ app/controllers/admins/admin_app_controller.rb
@@ -1,12 +1,4 @@
 class Admins::AdminAppController < ApplicationController
-  before_action :redirect_admin
+  before_action :authenticate_admin!
   layout 'admin'
-
-  private
-
-    def redirect_admin
-      unless current_admin
-        redirect_to root_path
-      end
-    end
 end
