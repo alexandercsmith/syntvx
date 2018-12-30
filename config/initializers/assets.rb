4b0870203bf2ca49519f8d5b0908252a84fcc64b
Alexander Smith <acsmith.dev@gmail.com>
2018-12-30 14:47:15 -0500

    update tool partial

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# 12-30-2018 : 2:00 AM : v1.26
+# 12-30-2018 : 3:00 PM : v1.27
 # Format: '#.##' | after #.#9 Increment to New Decedant | if .99 = New Full Version
-Rails.application.config.assets.version = '1.26'
+Rails.application.config.assets.version = '1.27'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
