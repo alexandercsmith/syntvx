780ca22b795df244702742e91a9cabadf492a710
Alexander Smith <acsmith.dev@gmail.com>
2018-12-29 22:19:14 -0500

    update admin sign in redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   http_basic_authenticate_with name: "admin", password: "$Duke123"
+
+  def after_sign_in_path_for(resource)
+    admins_path
+  end
 end
