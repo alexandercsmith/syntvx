f54496777aa0fe70ca8cb5098c62c7852f525c26
Alexander Smith <acsmith.dev@gmail.com>
2018-12-31 16:48:31 -0500

    update controllers for security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
 
+  http_basic_authenticate_with name: "admin", password: "$Duke69"
+
   def after_sign_in_path_for(resource)
     admins_path
   end
