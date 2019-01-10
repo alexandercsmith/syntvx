714fa6208a503af2041362c2f92312367f53dd98
Alexander Smith <acsmith.dev@gmail.com>
2019-01-10 18:55:35 -0500

    update admin icon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# 01-09-2018 : 07:00 PM : v1.39
+# 01-09-2018 : 07:00 PM : v1.40
 # Format: '#.##' | after #.#9 Increment to New Decedant | if .99 = New Full Version
-Rails.application.config.assets.version = '1.39'
+Rails.application.config.assets.version = '1.40'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
