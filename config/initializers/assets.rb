11fca59b6a97115e14db89d1f98132b476da259d
Alexander Smith <acsmith.dev@gmail.com>
2019-01-10 21:50:13 -0500

    update article and tool cards

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# 01-09-2018 : 09:00 PM : v1.46
+# 01-09-2018 : 09:30 PM : v1.47
 # Format: '#.##' | after #.#9 Increment to New Decedant | if .99 = New Full Version
-Rails.application.config.assets.version = '1.46'
+Rails.application.config.assets.version = '1.47'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
