437ab2861c058baa234a91288ab9ef95f245de1b
Alexander Smith <acsmith.dev@gmail.com>
2019-01-10 23:02:59 -0500

    add fonts to app

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# 01-09-2018 : 10:00 PM : v1.49
+# 01-09-2018 : 11:00 PM : v1.50
 # Format: '#.##' | after #.#9 Increment to New Decedant | if .99 = New Full Version
-Rails.application.config.assets.version = '1.49'
+Rails.application.config.assets.version = '1.50'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
