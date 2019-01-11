876d874a49c3471608d73bf445f87c833baf7779
Alexander Smith <acsmith.dev@gmail.com>
2019-01-10 21:56:47 -0500

    update favicon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# 01-09-2018 : 09:30 PM : v1.47
+# 01-09-2018 : 09:30 PM : v1.48
 # Format: '#.##' | after #.#9 Increment to New Decedant | if .99 = New Full Version
-Rails.application.config.assets.version = '1.47'
+Rails.application.config.assets.version = '1.48'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
